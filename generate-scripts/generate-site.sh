950f4641249ccd1fa46963866f6a004a68e135c8
Syndamia <kamen@syndamia.com>
2023-05-07 15:35:12 +0300

    [generate-site] Updated script management and changed AWK script scheme

--- generate-scripts/generate-site.sh
+++ generate-scripts/generate-site.sh
@@ -1,9 +1,27 @@
 #!/bin/sh
 
+# == Markdown source to HTML ==
+
 mkdir -p public
 ./ssg "$SOURCE_FOLDER" public "$SSG_TITLE" "$SSG_BASE_URL"
 
-find "$AWK_FOLDER" -type f -name "[A-Z]*.awk" -exec cat {} +  > ./script.awk
-find "$AWK_FOLDER" -type f -name "_*.awk"     -exec cat {} + >> ./script.awk
-find "$AWK_FOLDER" -type f -name "[a-z]*.awk" -exec cat {} + >> ./script.awk
-for file in $(find './public' -type f -name "*.html"); do awk -f 'script.awk' "$file" > tmp && mv -f tmp "$file"; done
+# == Editing HTML with AWK ==
+
+createAWKscript() {
+	# "librarires"
+	find "$AWK_FOLDER" -type f -name "#*.awk"     -exec cat {} +  > "$AWK_FILE"
+	# Scripts (before print)
+	find "$AWK_FOLDER" -type f -name "[A-Z]*.awk" -exec cat {} + >> "$AWK_FILE"
+	# Line print
+	echo '{if ($0 != 0) print}' >> "$AWK_FILE"
+	# Post-print scripts
+	find "$AWK_FOLDER" -type f -name "[a-z]*.awk" -exec cat {} + >> "$AWK_FILE"
+}
+runAWKscript() {
+	for file in $(find './public' -type f -name "*.html"); do
+		awk -f 'script.awk' "$file" > tmp && mv -f tmp "$file"
+	done
+}
+
+createAWKscript
+runAWKscript
